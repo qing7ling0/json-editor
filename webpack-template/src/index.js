3097e5a1914d28a84e883a8ee92e71e5915352c1
wuqingqing <wuqingqing070203@163.com>
2017-09-12 00:12:20 +0800

    webpack template

--- webpack-template/src/index.js
+++ webpack-template/src/index.js
@@ -1,15 +1,22 @@
 import _ from 'lodash';
 import './css/index.css';
+
+import printMe from './print.js'
+
 import Img1 from './images/test1.png'
 import Img2 from './images/test2.jpg'
 import Logo from './images/logo.png'
 
 function component() {
   var element = document.createElement('div');
-
-  // lodash 是由当前 script 脚本 import 导入进来的
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+     var btn = document.createElement('button');
+  
+      element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
+     btn.innerHTML = 'Click me and check the console!';
+     btn.onclick = printMe;
+  
+     element.appendChild(btn);
 
   var myIcon = new Image();
   myIcon.src = Img1;
--- webpack-template/webpack.config.js
+++ webpack-template/webpack.config.js
@@ -2,14 +2,21 @@
 
 const path = require('path')
 const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 const config = {
     entry: {
-        index:'./src/index.js'
+        index:'./src/index.js',
+        print: './src/print.js'
     },
     output: {
         filename: 'bundle.[name].js',
         path: path.join(__dirname, 'public')
+    },
+    devtool: 'source-map',
+    devServer: {
+        contentBase:path.join(__dirname, 'public')
     },
     module: {
         loaders: [
@@ -17,13 +24,17 @@ const config = {
             { 
                 test:/\.(png|svg|jpe?g|gif)$/i, 
                 loaders:[
-                    'file-loader',
+                    'url-loader?limit=10000',
                     'image-webpack-loader?{mozjpeg:{progressive:true}, gifsicle:{interlaced: false}, pngquant:{quality: "65-90", speed: 4}}',
                 ] 
             }
         ]
     },
     plugins: [
+        new HtmlWebpackPlugin({
+             title: 'Output Management'
+        }),
+        new CleanWebpackPlugin(['public'])
     ]
   };
   
