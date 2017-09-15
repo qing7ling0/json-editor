b01e3a764a3019e5e66cf585cf7075cc6c4dc1cd
qing7ling0 <wuqingqing070203@163.com>
2017-09-15 18:27:58 +0800

    webpack template

--- web/src/ConfigureStore.js
+++ web/src/ConfigureStore.js
@@ -5,9 +5,25 @@ import thunk from 'redux-thunk';//引入异步操作
 import reduxPromiseMiddleware from 'redux-promise';
 //引入所有的reducers,切记要在index.js封装下.
 import reducers from './reducers/index';
+
+const ReduxBegan = ({dispatch, getState}) => next => action => {
+  if (action && action.state !== 1) {
+    dispatch({type:action.type, state:1})
+  }
+  next(action);
+}
+const ReduxEnd = ({dispatch, getState}) => next => action => {
+  if (action && action.state !== 1) {
+    action.state = 3
+  }
+  next(action);
+}
+
 const middlewares = [
+  ReduxBegan,
   thunk,
-  reduxPromiseMiddleware
+  reduxPromiseMiddleware,
+  ReduxEnd
 ];
  
 const createSoreWithMiddleware=applyMiddleware(...middlewares)(createStore);
